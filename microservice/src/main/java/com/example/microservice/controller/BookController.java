7c409ec2dd5e098e5151ec89b27416a18b651068
xyz-3 <yichen_X1@outlook.com>
2023-10-26 10:52:17 +0800

    test

--- microservice/src/main/java/com/example/microservice/controller/BookController.java
+++ microservice/src/main/java/com/example/microservice/controller/BookController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class BookController {
     @Autowired
