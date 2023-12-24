8b7b8f04dd140fb6e980752709f342c2ecdbf260
xyz-3 <yichen_X1@outlook.com>
2023-12-24 22:14:35 +0800

    add spark

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -5,16 +5,32 @@ import com.example.bookstore.entity.BookTag;
 import com.example.bookstore.repository.BookTagRepository;
 import com.example.bookstore.service.BookService;
 import com.example.bookstore.util.msgutils.Msg;
+import com.example.bookstore.util.msgutils.MsgCode;
+import com.example.bookstore.util.msgutils.MsgUtil;
 import com.example.bookstore.util.response.BookForm;
 import com.example.bookstore.util.response.GraphBook;
+import net.sf.json.JSONObject;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+import org.bson.json.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 
 @RestController
 public class BookController {
@@ -98,5 +114,26 @@ public class BookController {
             return null;
         }
         return new GraphBook(book.getId(), book.getTitle(), book.getAuthor(), book.getPrice());
+    }
+
+    @RequestMapping(value = "/spark", method = RequestMethod.GET)
+    public Msg testSpark() throws IOException, InterruptedException {
+        //调用脚本文件
+        String command = "cmd /c spark-submit --class SimpleApp --master local[2] D:\\Projects\\se3353_25_spark_java\\target\\se3353_25_spark_java-1.0-SNAPSHOT.jar 10";
+        Process process = Runtime.getRuntime().exec(command, null, new File("D:\\spark-3.5.0-bin-hadoop3\\bin\\"));
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // read file
+        String filename = "D:\\spark-3.5.0-bin-hadoop3\\data\\bookOutput.txt";
+        Stream<String> lines = Files.lines(Paths.get(filename));
+        List<String> list = new ArrayList<>();
+        lines.forEach(list::add);
+        lines.close();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("data", list);
+        return MsgUtil.makeMsg(MsgCode.SUCCESS, MsgUtil.SUCCESS_MSG, jsonObject);
     }
 }
